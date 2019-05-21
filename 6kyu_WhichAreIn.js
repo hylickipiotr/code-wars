604d8541b6dec5ab48454525200f4d1c7372d255
nexus0212 <pietrek0212@gmail.com>
2019-05-21 21:24:12 +0200

    :sparkler: Solved 6kyu Which Are In

---
+++ 6kyu_WhichAreIn.js
@@ -0,0 +1,7 @@
+function inArray(array1,array2){
+  return array1.filter(word => array2.filter(w => w.includes(word)).length).sort();
+}
+
+console.log(inArray(["xyz", "live", "strong"], ["lively", "alive", "harp", "sharp", "armstrong"]), ["live", "strong"]);
+console.log(inArray(["live", "strong", "arp"], ["lively", "alive", "harp", "sharp", "armstrong"]), ["arp", "live", "strong"]);
+console.log(inArray(["tarp", "mice", "bull"], ["lively", "alive", "harp", "sharp", "armstrong"]), []);
